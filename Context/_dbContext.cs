d2dc186e91f5cf238ea498526208db51eeaa5a3d
Amila Prageeth <amila.perumbuli.work@gmail.com>
2022-10-23 09:29:00 +0530

    Database context

---
+++ Context/_dbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GoodProduct.Context
+{
+    public class _dbContext : DbContext
+    {
+        public _dbContext(DbContextOptions<_dbContext> options) : base(options)
+        {
+        }
+    }
+}
