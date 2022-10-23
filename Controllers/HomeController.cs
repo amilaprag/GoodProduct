f837ac839c8e6f0efdde329451a6da263a4c0c8a
Amila Prageeth <amila.perumbuli.work@gmail.com>
2022-10-23 07:25:20 +0530

    initial

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,21 @@ namespace GoodProduct.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View("Contact");
+        }
+
+        public IActionResult Client()
+        {
+            return View("Client");
+        }
+
+        public IActionResult Products()
+        {
+            return View("Products");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
